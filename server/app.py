2284705cd6aada6bf05a0f3d85de529a09b6a0f0
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-07-02 11:15:57 +0300

    All Tests Passed

--- server/app.py
+++ server/app.py
@@ -1,38 +1,54 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, abort
 from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return '<h1>Bakery GET API</h1>'
+    return "<h1>Bakery GET API</h1>"
 
-@app.route('/bakeries')
+
+@app.route("/bakeries")
 def bakeries():
-    return ''
+    bakeries = db.session.query(Bakery).all()
+    bakery_list = [bakery.to_dict() for bakery in bakeries]
+    return jsonify(bakery_list), 200
 
-@app.route('/bakeries/<int:id>')
+
+@app.route("/bakeries/<int:id>")
 def bakery_by_id(id):
-    return ''
+    bakery = db.session.get(Bakery, id) 
+    if not bakery:
+        abort(404)
+    return jsonify(bakery.to_dict()), 200  
 
-@app.route('/baked_goods/by_price')
+
+@app.route("/baked_goods/by_price")
 def baked_goods_by_price():
-    return ''
+    baked_goods = db.session.query(BakedGood).order_by(BakedGood.price.desc()).all()
+    return jsonify([bg.to_dict() for bg in baked_goods]), 200
 
-@app.route('/baked_goods/most_expensive')
+
+@app.route("/baked_goods/most_expensive")
 def most_expensive_baked_good():
-    return ''
+    most_expensive = (
+        db.session.query(BakedGood).order_by(BakedGood.price.desc()).first()
+    )
+    if not most_expensive:
+        abort(404)
+    return jsonify(most_expensive.to_dict()), 200
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
